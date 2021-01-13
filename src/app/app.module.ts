0fa247b0a1baf4ade002d040cdf8835d4b1a4305
Przemysław Matkowski <matkowski1994@gmail.com>
2021-01-13 20:48:36 +0100

    initial files/homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
